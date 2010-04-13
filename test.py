f84550eb6408dce00cf3efcb2a8d3b30407f12de
Ben Kaplan <benjamin.kaplan@case.edu>
2010-04-13 18:39:10 -0400

    Added the first few test cases as well as the script to run them.
    The tests are in "tests" (conveniently enough) and are placed one per line. Each line consists of a test case, a tab, and then the answer (if you're adding test cases, make sure you do an actual tab character and not a bunch of spaces). test.py (probably should rename it to avoid conflicts) will run each test case and check the answer for accuracy to 10 decimal places. It must be run in Python 2.6 or 3.x.

---
+++ test.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import subprocess
+import os.path
+EPSILON = 1e-10
+def main() :
+    dir = os.path.abspath(os.path.dirname(__file__))
+    test_cases = open(dir + '/tests', 'r')
+    proc = subprocess.Popen(['spim','-f',dir +'/calc.s'], 
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #get rid of the first part
+    for i in range(5) :
+        proc.stdout.readline()
+    for test in test_cases.readlines() :
+        input, result = test.split('\t')
+        proc.stdin.write(input.encode('utf-8') + b'\n')
+        proc.stdin.flush()
+        actual = proc.stdout.readline().strip()
+        print(input, '=',actual.decode('utf-8'),':',abs(float(result)-float(actual)) < EPSILON)
+    test_cases.close()
+    proc.communicate(b'quit\n')
+
+if __name__ == '__main__' :
+    main()
